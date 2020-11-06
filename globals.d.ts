3f4f067fc5a1bfc6f779ce11fa5ff2fde997682e
Yuu <okumura6883@gmail.com>
2020-11-05 17:44:37 -0800

    Updated types for API request

--- globals.d.ts
+++ globals.d.ts
@@ -1,12 +1,19 @@
+declare type UserRole = "guest" | "individual" | "organization";
+
+interface Client {
+    registered: boolean;
+    role:       UserRole;
+    // TODO: define other fields of a user
+}
+
 declare type APIRequest = import("express").Request & {
-    permissions: number;
-    user: any; // TODO
+    user: Client;
 };
 
 declare type APIResponse = import("express").Response;
 
 interface APIEndpointHandler {
-    handler(this: import("./src/server"), req: APIRequest, res: APIResponse): void;
+    handler(this: import("./src/server/index"), req: APIRequest, res: APIResponse): void;
     method: "get" | "post" | "patch" | "put" | "delete" | "head" | "options" | "use";
     path: string;
     pre?: Array<import("express").Handler>;
