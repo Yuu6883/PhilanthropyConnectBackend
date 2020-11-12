caead137038a9d9e0d8f6801875b339f37e10711
Yuu <okumura6883@gmail.com>
2020-11-11 16:54:44 -0800

    Added zip to location json

---
+++ data/parse.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+// data is from https://www.census.gov/geographies/reference-files/time-series/geo/gazetteer-files.html
+const lines = fs.readFileSync("data.txt", "utf8").split("\n").slice(1).filter(s => s);
+
+const out = {};
+
+lines.forEach(line => {
+    const [zip, a, b, c, d, lat, long] = line.split("\t");
+    out[zip] = [lat, long].map(Number);
+});
+
+fs.writeFileSync("zip.json", JSON.stringify(out));
