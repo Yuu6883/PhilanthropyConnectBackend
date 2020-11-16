5990cab8794d4d9e1d903f6dd851bc9a1907c097
Astronomity <andrewliangsd@gmail.com>
2020-11-15 17:35:52 -0800

    Finished organization tests and implementation

--- src/database/organizations.js
+++ src/database/organizations.js
@@ -3,14 +3,30 @@ const ZIP = require("../modules/us-zip");
 const Joi = require("joi");
 
 const validZIP = ZIP.map.keys();
+const validCauses = ["exampleCause", "Disaster Response"];
 
 // TODO: define template types in globals.d.ts and object schema
 /** @type {Joi.ObjectSchema<any>} */
 const schema = Joi.object({
+    title: Joi.string()
+        .required(),
+    mission: Joi.string()
+        .required(),
+    cause: Joi.array().items(
+        Joi.string()
+            .valid(...validCauses)
+    ),
     zip: Joi.string()
         .valid(...validZIP)
         .required()
-        .error(() => new Error("Invalid US zip code"))
+        .error(() => new Error("Invalid US zip code")),
+    org_contact: Joi.string()
+        .required(),
+    org_url: Joi.string()
+        .required(),
+    events: Joi.array().items(
+        Joi.string()
+    )
 });
 
 /**
@@ -33,8 +49,14 @@ class Organizations extends Template {
      * @returns {}
      */
     create(form) {
-        // TODO: transform the form (add location etc)
-        return form;
+        
+        /** @type {OrganizationDocument} */
+        const doc = form;
+        
+        // Default to empty array
+        doc.ratings = doc.ratings || [];
+        doc.followers = doc.followers || [];
+        return doc;
     }
 
     /**
--- test/organization.js
+++ test/organization.js
@@ -8,14 +8,131 @@ describe("Basic Organization Test", async function() {
     after(async () => await app.stop());
     
     it("Organization form validation", () => {
-        
+        const invalid_form = {};
+        let res = app.db.orgs.validate(invalid_form);
+        assert(!!(res.error || res.errors), "Form shouldn't work");
+
+        const empty_title = {
+            title: "",
+            mission: "Fixing broken programmers",
+            cause: [],
+            zip: "92037",
+            org_contact: "testemail@brokenprogrammers.org",
+            org_url: "yuh.org",
+            events: ["Sample event"]
+        };
+        res = app.db.orgs.validate(empty_title);
+        assert(!!(res.error || res.errors), "Expecting error");
+
+        const empty_mission = {
+            title: "Yuh",
+            mission: "",
+            cause: [],
+            zip: "92037",
+            org_contact: "testemail@brokenprogrammers.org",
+            org_url: "yuh.org",
+            events: ["Sample event"]
+        };
+        res = app.db.orgs.validate(empty_mission);
+        assert(!!(res.error || res.errors), "Expecting error");
+
+        const empty_contact = {
+            title: "Yuh",
+            mission: "Fixing broken programmers",
+            cause: [],
+            zip: "92037",
+            org_contact: "",
+            org_url: "yuh.org",
+            events: ["Sample event"]
+        };
+        res = app.db.orgs.validate(empty_contact);
+        assert(!!(res.error || res.errors), "Expecting error");
+
+        const empty_url = {
+            title: "Yuh",
+            mission: "Fixing broken programmers",
+            cause: [],
+            zip: "92037",
+            org_contact: "testemail@brokenprogrammers.org",
+            org_url: "",
+            events: ["Sample event"]
+        };
+        res = app.db.orgs.validate(empty_url);
+        assert(!!(res.error || res.errors), "Expecting error");
+
+        const validForm = {
+            title: "Yuh",
+            mission: "Fixing broken programmers",
+            cause: [],
+            zip: "92037",
+            org_contact: "testemail@brokenprogrammers.org",
+            org_url: "yuh.org",
+            events: []
+        };
+        res = app.db.orgs.validate(validForm);
+        assert.ifError(res.error || res.errors);
+
     });
 
-    it("Database tests", async() => {
-        
+    it("Organization Database CRUD tests", async() => {
+        const testID = `org-test-${Date.now()}`;
+
+        let none = await app.db.orgs.byID(testID);
+        assert(!none.exists, "No document expected");
+
+        let deleteRes = await app.db.orgs.delete(testID);
+        assert(!deleteRes, "No document should be deleted");
+
+        const valid_form = {
+            title: "Yuh",
+            mission: "Fixing broken programmers",
+            cause: [],
+            zip: "92037",
+            org_contact: "testemail@brokenprogrammers.org",
+            org_url: "yuh.org",
+            events: []
+        };
+        let doc = app.db.orgs.create(valid_form);
+        doc.id = testID;
+
+        const ref = await app.db.orgs.insert(doc);
+        const snapshot = await ref.get();
+        // Create successful
+        assert(snapshot.exists, "Document should be inserted");
+        assert(snapshot.id == testID, "ID should match test ID");
+
+        // Read document
+        const readDoc = snapshot.data();
+        assert(readDoc.zip == doc.zip, "Document zip should match");
+        const oldLocation = readDoc.zip;
+
+        // Update document
+        const updated_form = {
+            title: "Yuh",
+            mission: "Fixing broken programmers",
+            cause: [],
+            zip: "92122",
+            org_contact: "testemail@brokenprogrammers.org",
+            org_url: "yuh.org",
+            events: ["new event"]
+        };
+        doc = app.db.orgs.create(updated_form);
+        const updated = await app.db.orgs.update(testID, doc);
+        assert(updated, "Update operation should be successful");
+
+        const readUpdatedDoc = (await app.db.orgs.byID(testID)).data();
+        const newLocation = readUpdatedDoc.zip;
+        assert(!(newLocation == oldLocation), "Location should be updated");
+
+        // Delete document
+        deleteRes = await app.db.orgs.delete(testID);
+        assert(deleteRes, "Document should be deleted");
+
+        none = await app.db.orgs.byID(testID);
+        assert(!none.exists, "No document expected");
     });
 
-    it("Database mock test", async() => {
+    it("Organization Database mock test", async() => {
     });
 
 });
