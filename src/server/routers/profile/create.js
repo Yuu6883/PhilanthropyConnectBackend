5ae28266f433546916107568c3aea2d4bb1836a0
Yuu <okumura6883@gmail.com>
2020-11-19 03:32:24 -0800

    Individual & Organization create tests all passed

--- src/server/routers/profile/create.js
+++ src/server/routers/profile/create.js
@@ -4,49 +4,26 @@ module.exports = {
     path: "/profile/create",
     handler: async function (req, res) {
         // Design use case 2.1
-        // TODO: validate user registration form and create either an individual or an organization record
+        /** @type {"individual"|"organization"} */
         const type = req.query.type;
 
         if (await this.db.existsProfile(req.payload.uid)) {
             return res.status(400).send({ error: "Profile already exists with the assoicated account" });
         }
 
-        if (type == "individual") {
+        const db = { "individual": this.db.inds, "organization": this.db.orgs }[type];
+        
+        if (!db) return res.sendStatus(400);
 
-            const validatedForm = this.db.inds.schema.validate(req.body);
-            if (validatedForm.error || validatedForm.errors) {
-                console.error(validatedForm.error || validatedForm.errors);
-                return res.sendStatus(400);
-            }
-            
-            let doc = this.db.inds.formToDocument(validatedForm.value);
-            
-            doc.id = req.payload.uid;
-            doc.email = req.payload.email;
+        const validatedForm = db.schema.validate(req.body);
+        if (validatedForm.error || validatedForm.errors) return res.sendStatus(400);
+        
+        let doc = db.formToDocument(validatedForm.value);
+        
+        doc.id = req.payload.uid;
+        doc.email = req.payload.email;
 
-            await this.db.inds.insert(doc);
-            return res.send({ success: true });
-
-        } else if (type == "organization") {
-
-            // Organization implementation of create goes here
-            const validatedForm = this.db.orgs.schema.validate(req.body);
-            console.log(validatedForm.error || validatedForm.errors);
-            if (validatedForm.error || validatedForm.errors) return res.sendStatus(400);
-
-            let doc = this.db.orgs.formToDocument(validatedForm.value);
-
-            doc.id = req.payload.uid;
-            doc.email = req.payload.email;
-
-            await this.db.orgs.insert(doc);
-            return res.send({ success: true });
-
-        } else return res.sendStatus(400);
-
-        // TODO: Create an organization record
-        // const validatedDocument = this.db.orgs.formToDocument(validatedForm);
-        // await this.db.orgs.insert(validatedForm);
-        // return res.sendStatus(200);
+        await db.insert(doc);
+        return res.send({ success: true });
     }
 }
