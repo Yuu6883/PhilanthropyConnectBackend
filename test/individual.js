3827768af42b3f653e7dd2c654d410b9d8ecebf7
Yuu <okumura6883@gmail.com>
2020-11-19 06:08:12 -0800

    Replaced snake case and finished individual tests

--- test/individual.js
+++ test/individual.js
@@ -21,11 +21,11 @@ describe("Basic Individual Test", async function() {
      * Age: Must be one of the valid age ranges, required
      */
     it("Individual User form validation", () => {
-        const invalid_form = {};
-        let res = app.db.inds.validate(invalid_form);
+        const invalidForm = {};
+        let res = app.db.inds.validate(invalidForm);
         assert(!!(res.error || res.errors), "Expecting error");
 
-        const empty_first = {
+        const emptyFirstnameForm = {
             firstname: "",
             lastname: "Beihl",
             cause: [],
@@ -33,10 +33,10 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: "20-29"
         };
-        res = app.db.inds.validate(empty_first);
+        res = app.db.inds.validate(emptyFirstnameForm);
         assert(!!(res.error || res.errors), "Expecting error");
 
-        const invalid_first = {
+        const invalidFirstnameForm = {
             firstname: "Branson@",
             lastname: "Beihl",
             cause: [],
@@ -44,10 +44,10 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: "20-29"
         };
-        res = app.db.inds.validate(invalid_first);
+        res = app.db.inds.validate(invalidFirstnameForm);
         assert(!!(res.error || res.errors), "Expecting error");
 
-        const empty_last = {
+        const emptyLastnameForm = {
             firstname: "Branson@",
             lastname: "Beihl",
             cause: [],
@@ -55,10 +55,10 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: "20-29"
         };
-        res = app.db.inds.validate(empty_last);
+        res = app.db.inds.validate(emptyLastnameForm);
         assert(!!(res.error || res.errors), "Expecting error");
 
-        const invalid_last = {
+        const invalidLastnameForm = {
             firstname: "Branson",
             lastname: "Beihl@",
             cause: [],
@@ -66,10 +66,10 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: "20-29"
         };
-        res = app.db.inds.validate(invalid_last);
+        res = app.db.inds.validate(invalidLastnameForm);
         assert(!!(res.error || res.errors), "Expecting error");
 
-        const empty_cause = {
+        const emptyCauseForm = {
             firstname: "Branson",
             lastname: "Beihl",
             cause: [],
@@ -77,10 +77,10 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: "20-29"
         };
-        res = app.db.inds.validate(empty_cause);
+        res = app.db.inds.validate(emptyCauseForm);
         assert.ifError(res.error || res.errors);
 
-        const invalid_cause = {
+        const invalidCauseForm = {
             firstname: "Branson",
             lastname: "Beihl",
             cause: ["Invalid Cause"],
@@ -88,10 +88,10 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: "20-29"
         };
-        res = app.db.inds.validate(invalid_cause);
+        res = app.db.inds.validate(invalidCauseForm);
         assert(!!(res.error || res.errors), "Expecting error");
 
-        const empty_zip = {
+        const emptyZipForm = {
             firstname: "Branson",
             lastname: "Beihl",
             cause: ["Disaster Response"],
@@ -99,10 +99,10 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: "20-29"
         };
-        res = app.db.inds.validate(empty_zip);
+        res = app.db.inds.validate(emptyZipForm);
         assert(!!(res.error || res.errors), "Expecting error");
 
-        const invalid_zip = {
+        const invalidZipForm = {
             firstname: "Branson",
             lastname: "Beihl",
             cause: ["Disaster Response"],
@@ -110,10 +110,10 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: "20-29"
         };
-        res = app.db.inds.validate(invalid_zip);
+        res = app.db.inds.validate(invalidZipForm);
         assert(!!(res.error || res.errors), "Expecting error");
 
-        const empty_skills = {
+        const emptySkillsForm = {
             firstname: "Branson",
             lastname: "Beihl",
             cause: ["Disaster Response"],
@@ -121,10 +121,10 @@ describe("Basic Individual Test", async function() {
             skills: [],
             age: "20-29"
         };
-        res = app.db.inds.validate(empty_skills);
+        res = app.db.inds.validate(emptySkillsForm);
         assert.ifError(res.error || res.errors);
         
-        const empty_age = {
+        const emptyAgeForm = {
             firstname: "Branson",
             lastname: "Beihl",
             cause: ["Disaster Response"],
@@ -132,10 +132,10 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: ""
         };
-        res = app.db.inds.validate(empty_age);
+        res = app.db.inds.validate(emptyAgeForm);
         assert(!!(res.error || res.errors), "Expecting error");
 
-        const invalid_age = {
+        const invalidAgeForm = {
             firstname: "Branson",
             lastname: "Beihl",
             cause: ["Disaster Response"],
@@ -143,10 +143,10 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: "20-295837"
         };
-        res = app.db.inds.validate(invalid_age);
+        res = app.db.inds.validate(invalidAgeForm);
         assert(!!(res.error || res.errors), "Expecting error");
 
-        const valid_form = {
+        const validForm = {
             firstname: "Branson",
             lastname: "Beihl",
             cause: ["Disaster Response"],
@@ -154,7 +154,7 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: "20-29"
         };
-        res = app.db.inds.validate(valid_form);
+        res = app.db.inds.validate(validForm);
         assert.ifError(res.error || res.errors);
         
         // Do stuff with res.value
@@ -169,7 +169,7 @@ describe("Basic Individual Test", async function() {
         let deleteRes = await app.db.inds.delete(testID);
         assert(!deleteRes, "No document should be deleted");
 
-        const valid_form = {
+        const validForm = {
             firstname: "Branson",
             lastname: "Beihl",
             cause: ["Disaster Response"],
@@ -177,7 +177,7 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: "20-29"
         };
-        let doc = app.db.inds.formToDocument(valid_form);
+        let doc = app.db.inds.formToDocument(validForm);
         doc.id = testID;
 
         const ref = await app.db.inds.insert(doc);
@@ -192,7 +192,7 @@ describe("Basic Individual Test", async function() {
         const oldLocation = readDoc.location;
 
         // Update document
-        const updated_form = {
+        const updatedForm = {
             firstname: "Branson",
             lastname: "Beihl",
             cause: ["Disaster Response"],
@@ -200,7 +200,7 @@ describe("Basic Individual Test", async function() {
             skills: ["exampleSkill"],
             age: "20-29"
         };
-        doc = app.db.inds.formToDocument(updated_form);
+        doc = app.db.inds.formToDocument(updatedForm);
         const updated = await app.db.inds.update(testID, doc);
         assert(updated, "Update operation should be successful");
 
@@ -216,18 +216,18 @@ describe("Basic Individual Test", async function() {
         assert(!none.exists, "No document expected");
     });
 
-    it("Individual Endpoint CRUD test", async() => {
+    it("Individual Endpoint CRUD tests", async () => {
 
         const testPayload = app.testPayload = {
             "uid": `indi-test-${Date.now()}`,
             "name": "Branson Beihl",
             "picture": "",
             "email": "example@ucsd.edu",
-            "email_verified": true
+            "emailVerified": true
         };
 
-        // frontend form to create
-        const test_auth_form = {
+        // Frontend form to create
+        const testAuthForm = {
             firstname: "Branson",
             lastname: "Beihl",
             cause: ["Disaster Response"],
@@ -236,30 +236,126 @@ describe("Basic Individual Test", async function() {
             age: "20-29"
         };
 
+        // Valid test
         /** @type {Response} */
         let res = await fetch(`http://localhost:${app.config.port}/api/profile/create?type=individual`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(test_auth_form)
+            body: JSON.stringify(testAuthForm)
         });
-
         assert(res.status == 200, `Valid form should return http status 200 instead of ${res.status}`);
-
         const data = (await app.db.inds.byID(testPayload.uid)).data();
-        assert(data.age == test_auth_form.age, "Age should match in the submitted document");
+        assert(data.age == testAuthForm.age, "Age should match in the submitted document");
 
-        /** @type {Response} */
+        // Post to existing profile
         res = await fetch(`http://localhost:${app.config.port}/api/profile/create?type=individual`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(test_auth_form)
+            body: JSON.stringify(testAuthForm)
         });
-
         assert(res.status == 400, `Already existing profile should return 400 instead of ${res.status}`);
 
-        await app.db.inds.delete(testPayload.uid);
+        // GET the profile we created through the first test
+        res = await fetch(`http://localhost:${app.config.port}/api/profile/${testPayload.uid}?type=individual`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        });
+        assert(res.status == 200, `Profile GET on created document endpoint should return 200 instead of ${res.status}`);
+        let jsonRes = await res.json();
+        assert(jsonRes.success && jsonRes.profile.age == testAuthForm.age, "Response should be successful and the fields matches");
 
-        // TODO: test other individual endpoints
+        // GET the profile we created through the first test, but with type=organization query string
+        res = await fetch(`http://localhost:${app.config.port}/api/profile/${testPayload.uid}?type=organization`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        });
+        assert(res.status == 404, `Profile GET on endpoint with type=organization should return 404 instead of ${res.status}`);
+
+        // GET the profile we created through the first test, but without query string
+        res = await fetch(`http://localhost:${app.config.port}/api/profile/${testPayload.uid}`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        });
+        assert(res.status == 400, `Profile GET on endpoint without query string should return 400 instead of ${res.status}`);
+
+        // GET the user's profile with @me
+        res = await fetch(`http://localhost:${app.config.port}/api/profile/@me`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        });
+        assert(res.status == 200, `Profile GET on @me endpoint should return 200 instead of ${res.status}`);
+        jsonRes = await res.json();
+        assert(jsonRes.success && jsonRes.profile.age == testAuthForm.age, "Response should be successful and the fields matches");
+
+        // Remove our test authorization from app
+        delete app.testPayload; 
+        // GET the user's profile with @me but NOT authorized (without payload)
+        res = await fetch(`http://localhost:${app.config.port}/api/profile/@me`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        });
+        assert(res.status == 401, `Profile GET on @me without authorization should return 401 instead of ${res.status}`);
+
+        // Put our test authorization back to app
+        app.testPayload = testPayload;
+
+        // Update tests
+        // PUT to not @me
+        res = await fetch(`http://localhost:${app.config.port}/api/profile/otherUser`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(testAuthForm)
+        });
+        assert(res.status == 403, `Profile PUT on wrong endpoint after /api/profile should return 403 instead of ${res.status}`);
+
+        const testOrgForm = {
+            title: "Habitat For Humanity",
+            mission: "Fixing homelessness one family at a time",
+            cause: ["Disaster Response"],
+            zip: "92037",
+            contact: "info@sandiegohabitat.org",
+            url: "www.sandiegohabitat.org",
+            events: []
+        };
+        
+        const testUpdatedForm = {
+            firstname: "Branson",
+            lastname: "Beihl",
+            cause: ["Disaster Response"],
+            zip: "92037", // we updated the zip
+            skills: ["exampleSkill"],
+            age: "20-29"
+        };
+
+        // PUT to @me with wrong type of profile (user somehow wants to 
+        // hack their profile to become an organization or frontend error)
+        res = await fetch(`http://localhost:${app.config.port}/api/profile/@me?type=organization`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(testOrgForm)
+        });
+        assert(res.status == 403, `Profile PUT on @me without authorization should return 403 instead of ${res.status}`);
+
+        // PUT to @me
+        res = await fetch(`http://localhost:${app.config.port}/api/profile/@me?type=individual`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(testUpdatedForm)
+        });
+        jsonRes = await res.json();
+        assert(res.status == 200 && jsonRes.success, `Profile update should be successful (status: ${res.status})`);
+
+        // GET the user's profile with @me (to test if the document updated)
+        res = await fetch(`http://localhost:${app.config.port}/api/profile/@me`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        });
+        assert(res.status == 200, `Profile GET on @me endpoint should return 200 instead of ${res.status}`);
+        jsonRes = await res.json();
+        assert(jsonRes.success && jsonRes.profile.zip == testUpdatedForm.zip, "Response should have updated zip");
+
+        // TODO: make this run even when the test fails
+        await app.db.inds.delete(testPayload.uid);
     });
 
 });
