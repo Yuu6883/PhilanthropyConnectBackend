716385ec7ff18becac22ad23cf088496653e21e9
Vivian Chiong <vchiong@ucsd.edu>
2020-11-29 14:23:42 -0800

    feed.js first draft done :0

--- src/server/routers/organization/feed.js
+++ src/server/routers/organization/feed.js
@@ -23,33 +23,38 @@ module.exports = {
             const indi = await this.db.inds.byID(req.payload.uid);
             if (!indi.exists) return res.sendStatus(403);
             const indiDoc = indi.data();
-            const indiFollowing = indiDoc.following;
 
-            // TODO: For each org, get the org's events
-            // Map the org id's to their document?
+            // Map the org id's to their document
+            const query = await Promise.all(indiDoc.following.map(id => this.db.orgs.byID(id)));
+            let followedOrgs  = query.map(org => org.data());
+
+            /** @type {OrgEventDocument[]} */
+            const allEvents = [];  // all events from followed orgs
+            
+            // For each org, add their events to final allEvents array
+            for (orgDoc in followedOrgs) {
+  
+                const events = await Promise.all(orgDoc.events.map(id => this.db.events.byID(id)));
+                let eventDocs  = events.map(org => org.data());
+                
+                allEvents = allEvents || eventDocs;
+            }
+
+            // Sort OrgEventDocuments by date: lowest timestamp first
+            allEvents.sort(function(a, b) {return a.date - b.date});
+
+            // Get 50 most recent events from allEvents to send back as response
+            res.send(allEvents.slice(0, 49));
 
         // If db is org: send back array of their own events' documents
         } else if (db == Organizations) {
 
-            // Get the org's events
             const org = await this.db.orgs.byID(req.payload.uid);
             if (!org.exists) return res.sendStatus(403);
             const orgDoc = org.data();
 
-            // Map the event id's to their document
             const events = await Promise.all(orgDoc.events.map(id => this.db.events.byID(id)));
-            res.send(events.map(r => r.data()));
-
-            /* Trash code
-            orgEvents.forEach(eventID => {
-                let eventRes = await fetch(`http://localhost:${app.config.port}/api/events/${eventID}`, {
-                    method: "GET"
-                });
-                if (!eventRes.exists) return res.sendStatus(404);
-                let finalJSON = finalJSON | {eventRes};
-            });
-            res.send(finalJSON);
-            */
+            res.send(events.map(orgEvent => orgEvent.data()));
         }
     }
 }
