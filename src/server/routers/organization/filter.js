bfa1d1f5fff2456b6fe99c9070395b6cc07b55cc
Astronomity <andrewliangsd@gmail.com>
2020-11-30 03:30:55 -0800

    Made an asynchronous search filter

--- src/server/routers/organization/filter.js
+++ src/server/routers/organization/filter.js
@@ -1,6 +1,6 @@
 const { validCauses, validSkills } = require("../../../constants");
 const Joi = require("joi");
-const { withinRadius } = require("geolib");
+const { isPointWithinRadius } = require("geolib");
 const ZIPCodes = require("../../../modules/us-zip");
 const { GeoPoint } = require("@google-cloud/firestore");
 
@@ -13,7 +13,7 @@ module.exports = {
         // /api/organizations/filter?=["filter1","filter2","etc"]
         
         // Get the JSON object from URL
-        const decodedFilter = JSON.parse(decodeURIComponent(req._parsedUrl.query.substring(1)));
+        let decodedFilter = JSON.parse(decodeURIComponent(req._parsedUrl.query.substring(1)));
 
         /**
          * @type {Joi.ObjectSchema} 
@@ -23,7 +23,7 @@ module.exports = {
         const schema = Joi.object({
 
             // User calling from Causes or My Causes
-            followed: Joi.boolean()
+            myCauses: Joi.boolean()
                 .required(),
             
             // User wants to use the skills listed on their profile
@@ -55,89 +55,50 @@ module.exports = {
         // Validate the filter query
         const validatedForm = schema.validate(decodedFilter);
         if (validatedForm.error || validatedForm.errors) return res.sendStatus(400);
+        decodedFilter = validatedForm.value;
+        decodedFilter.causes = decodedFilter.causes || [];
+        decodedFilter.skills = decodedFilter.skills || [];
+        decodedFilter.distance = decodedFilter.distance || 50;
         
-        // Query only within followed orgs
-        if (decodedFilter.followed) {
-            // Query parameters: Defaults to all valid filters if parameter is empty
-            const causesQuery = decodedFilter.causes.length ? decodedFilter.causes : validCauses;
-            const skillsQuery = decodedFilter.mySkills ? user.skills : decodedFilter.skills ? decodedFilter.skills : validSkills;
-            const distQuery   = decodedFilter.distance ? decodedFilter.distance * 1.609 : 50 * 1.609;
+        // Query parameters: Defaults to all valid filters if parameter is empty
+        const causesQuery = decodedFilter.myCauses ? user.causes : (decodedFilter.causes.length) ? decodedFilter.causes : validCauses;
+        const skillsQuery = decodedFilter.mySkills ? user.skills : (decodedFilter.skills.length) ? decodedFilter.skills : validSkills;
+        const distQuery   = decodedFilter.distance * 1609; // Converts miles to meters
 
-            // Query db for all followed organizations and filter it based on given filters
-            const query       = await Promise.all(user.following.map(id => this.db.orgs.byID(id)));
-            let followedOrgs  = query.map(org => org.data());
-            const result = followedOrgs.filter(_ = (org) => {
-
-                /**
-                 * Search intended functionality:
-                 * - If any of the queries are empty, assume the org always meets
-                 *   this query requirement (if no causes specified then only filter by skill/distance)
-                 * - If say cause1 was a search parameter and 50 miles was a search parameter, then
-                 *   only orgs that include cause1 AND are within 50 miles are returned
-                 * - If cause1 and cause2 were search parameters, orgs with cause1 OR cause2 are
-                 *   returned
-                 * - If cause1 and cause2 and 50 miles were parameters, then orgs with cause1 OR
-                 *   cause2 AND within 50 miles are returned
-                 * - The cases extend to all the search parameters
-                 */
-
-                // Check for cause matches
-                retval = false;
-                for(key in causesQuery) {
-                    if(org.cause.includes(key)) {
-                        retval = true;
-                        break;
-                    }
-                }
-                
-                // Cause was required but org did not match
-                if(!retval) return false;
-                
-                // Check for skills matches
-                retval = false;
-                for(key in skillsQuery) {
-                    if(org.cause.includes(key)) {
-                        retval = true;
-                        break;
-                    }
-                }
-
-                // Check for distance matches (convert our miles to km to work with GeoLib)
-                orgLoc = new GeoPoint(...(ZIPCodes.map.get(org.zip)));
-                indLoc = new GeoPoint(...(ZIPCodes.map.get(user.zip)));
-                return retval ? withinRadius(orgLoc, indLoc, distQuery) : false;
+        // Query db for causes
+        let result      = [];
+        let query       = await this.db.orgs.ref.where("cause", "array-contains-any", causesQuery)
+            .get()
+            .then(_ = (querySnapshot) => {
+                querySnapshot.forEach(_ = (doc) => {
+                    result.push(doc.data());
+                });
             });
-            return res.send(result);
+        if(result.length == 0) return false;
         
-        // User is calling for all organizations
-        } else {
-            // Query parameters: Defaults to all valid filters if parameter is empty
-            const causesQuery = decodedFilter.causes.length ? decodedFilter.causes : validCauses;
-            const skillsQuery = decodedFilter.mySkills ? user.skills : decodedFilter.skills ? decodedFilter.skills : validSkills;
-            const distQuery   = decodedFilter.distance ? decodedFilter.distance * 1.609 : 50 * 1.609;
-
-            // Query db for causes
-            const query       = this.db.orgs.where("causes", "array-contains-any", causesQuery);
-            let   result      = query.map(org => org.data());
-
-            // Filter db for distance and skills
-            result            = result.filter( _ = (org) => {
-
-                // Check for skills filter
-                let retval = false;
-                for(key in skillsQuery) {
-                    if(org.skills.includes(key)) {
-                        retval = true;
-                        break;
-                    }
-                }
-                
-                // Check for distance matches (convert our miles to km to work with GeoLib)
-                orgLoc = new GeoPoint(...(ZIPCodes.map.get(org.zip)));
-                indLoc = new GeoPoint(...(ZIPCodes.map.get(user.zip)));
-                return retval ? withinRadius(orgLoc, indLoc, distQuery) : false;
-            });
-            return res.send(result);
+        const filteredResult = async (arr, predicate) => {
+            const results = await Promise.all(arr.map(predicate));
+            return results;
         }
+        const pred = await filteredResult(result, async(org) => {
+
+            // Check for skills filter
+            let retval = false;
+            let eventQuery = await Promise.all(org.events.map(id => this.db.events.byID(id)));
+            let orgEvents = eventQuery.map(i => i.data());
+            for(id of orgEvents) {
+                if(skillsQuery.some(i => id.skills.includes(i))) {
+                    retval = true;
+                    break;
+                }
+            }
+            // Check for distance matches (convert our miles to km to work with GeoLib)
+            orgLoc = { latitude: ZIPCodes.map.get(org.zip)[0], longitude: ZIPCodes.map.get(org.zip)[1] };
+            indLoc = { latitude: ZIPCodes.map.get(user.zip)[0], longitude: ZIPCodes.map.get(user.zip)[1] };
+            return retval ? isPointWithinRadius(orgLoc, indLoc, distQuery) : false;
+        });
+        
+        const results = result.filter((value, i) => pred[i]);
+        return res.send({body: results});
     }
 }
