1e49aedaaf0c3fbd164f66384a57904a8213400d
Astronomity <andrewliangsd@gmail.com>
2020-12-01 00:28:34 -0800

    Added pseudocode for recommend

--- src/server/routers/organization/recommend.js
+++ src/server/routers/organization/recommend.js
@@ -1,9 +1,12 @@
 /** @type {APIEndpointHandler} */
 module.exports = {
-    method: "get",
+    method: "GET",
     path: "/organization/recommend",
     handler: function (req, res) {
         // Design use case 6.1
         // TODO: get organization recommendations
+        // All frontend has to do is send a request here
+        // And all we have to do is get ALL the orgs database
+        // Just return X number of organizations that the user isn't already following
     }
 }
