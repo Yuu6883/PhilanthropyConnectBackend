c4dff5719ba4521810c28e4afe28aa74c68f3c3d
Branson Beihl <47403344+MissouriBoy@users.noreply.github.com>
2020-12-01 16:07:31 -0800

    Initial Follow Funct

--- src/server/routers/organization/follow.js
+++ src/server/routers/organization/follow.js
@@ -5,10 +5,21 @@ module.exports = {
     handler: function (req, res) {
         // Design use case 5.2
         // /organizations/id?follow=[true/false]
-
-        // validate it's a individual user in req.payload.uid
+        const type = req.query.follow;
+        
         // validate organization exists from req.params.id
+        const org = await this.db.orgs.byID(req.params.id);
+        if (!org.exists) return res.sendStatus(400);
+        const orgDoc = org.data();
+        // validate it's a individual user in req.payload.uid
+        const follower = await this.db.inds.byID(req.payload.uid);
+        if (!follower.exists) return res.sendStatus(401);
         // call addFollower
+        if(type) {
+            await this.db.orgs.addFollower(org, follower);
+        } else {
+            await this.db.orgs.removeFollower(org, follower);
+        }    
         // done!!
         
         // TODO: follow/unfollow organization
